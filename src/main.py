6e4226f9add6d23ca6b3897937019e427c353804
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2021-01-17 20:05:42 +0900

    キーボード入力により画像保存するかを選べるように

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import cv2
 
 # Load an color image in grayscale
 img = cv2.imread('sample.jpg', 0)
-cv2.imwrite('dist/sample.png', img)
 
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('image', img)
+key = cv2.waitKey(0)
+if key == ord('s'):  # s を押したら画像保存
+    cv2.imwrite('dist/sample.png', img)
+    cv2.destroyAllWindows()
+else: # 他のキーならウィンドウを閉じるだけ
+    cv2.destroyAllWindows()
