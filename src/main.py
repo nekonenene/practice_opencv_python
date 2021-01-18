aa5acc360f060ae050d5c2a6b596122b92ed0edd
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2021-01-19 00:35:52 +0900

    画像から顔の認識

---
+++ src/main.py
@@ -0,0 +1,39 @@
+# 参考:
+# https://blog.mudatobunka.org/entry/2016/10/03/014520
+# https://qiita.com/hitomatagi/items/04b1b26c1bc2e8081427
+import cv2
+
+# 画像の読み込み
+image = cv2.imread('sample.jpg')
+
+# 処理速度を高めるために画像をグレースケールに変換
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# 評価器を読み込み
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
+
+# 顔検出
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor = 1.2,
+    minNeighbors = 3,
+    minSize = (50, 50),
+)
+
+for (x, y, w, h) in faces:
+    # 検知した顔を矩形で囲む
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), 3)
+    # 顔画像（グレースケール）
+    roi_gray = gray[y : y + h, x : x + w]
+    # 顔画像（カラースケール）
+    roi_color = image[y : y + h, x : x + w]
+    # 顔の中から目を検知
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        # 検知した目を矩形で囲む
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+cv2.imshow('image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
